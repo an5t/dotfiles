5bf0514526ec37bc5826c680b73958398684f808
Andrey Mishakin <stieroglif@gmail.com>
2023-02-24 21:50:32 +0300

    nvim: add gopls

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -68,6 +68,11 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
     capabilities = capabilities,
   }
 
+  lspconfig['gopls'].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+
 end
 
 return M
