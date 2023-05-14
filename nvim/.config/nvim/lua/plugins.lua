3cc1a832cd194f783de565539e836403f28d884d
Andrey Mishakin <stieroglif@gmail.com>
2023-05-14 17:01:14 +0300

    nvim: indent-blankline: don't show current context

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -77,7 +77,7 @@ return require('packer').startup(function(use)
     'lukas-reineke/indent-blankline.nvim',
     config = function()
       require('indent_blankline').setup {
-        show_current_context = true,
+        show_current_context = false,
       }
     end
   }
