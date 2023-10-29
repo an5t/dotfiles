50fe4683dabe4fc01a8e55738b4c038e2986b3df
Andrey Mishakin <stieroglif@gmail.com>
2023-10-30 02:44:30 +0300

    nvim: disable netrw (recommended in nvim-tree docs)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- disable netrw (recommended in nvim-tree docs)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require 'settings'
 require 'keymaps'
 require 'plugins'
-require 'settings'
