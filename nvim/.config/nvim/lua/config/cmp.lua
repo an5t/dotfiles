3f44f07174aaa3dc888395400f4820b9235eba7d
Andrey Mishakin <stieroglif@gmail.com>
2023-10-29 21:52:51 +0300

    nvim: tune cmp behavior

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -13,10 +13,12 @@ return function()
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete {},
+
       ['<CR>'] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
       },
+
       ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
@@ -26,6 +28,7 @@ return function()
           fallback()
         end
       end, { 'i', 's' }),
+
       ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
