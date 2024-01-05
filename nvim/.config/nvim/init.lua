3b4c1b4d0fcc61cc57573c3f838b7418341b244f
Andrey Mishakin <stieroglif@gmail.com>
2024-01-05 20:19:08 +0300

    nvim: enable netrw since it breaks GBrowse in vim-fugitive
    https://github.com/tpope/vim-fugitive/issues/1010

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- disable netrw (recommended in nvim-tree docs)
-vim.g.loaded_netrw = 1
+--vim.g.loaded_netrw = 1 -- breaks :GBrowse in vim-fugitive
 vim.g.loaded_netrwPlugin = 1
 
 require("settings")
